33c84eaa4f6b17d2adc6012fdf480b46bc7fd608
Thomas Bouchard <thomaswardbouchard@gmail.com>
2018-02-26 15:09:15 +0100

    save form form into json file

---
+++ content.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todooz</title>
+</head>
+<body>
+     <h1>Todooz: The best todo-list ... like ever</h1>
+
+    <h2>Todooz</h2>
+  <form action="content.php" method="post">
+   Add a task <input type="text" name="todoitem" id="todoitem" /><br />
+     <input type="submit" id="submit" value="Send" />
+  </form>
+
+</body>
+</html>
--- form.php
+++ form.php
@@ -1,16 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+ $filetxt = 'todo.json';
+ if(isset($_POST['todoitem'])){
+     if(empty($_POST['todoitem'])){
+         echo 'Please write down a task, do not be afraid to commit!';
+     }
+     else{
+         $formdata = array(
+             'todoitem' => $_POST['todoitem']);
+        $filetxt = 'todo.json';
+
+        // pull data from json file before adding nex data into it
+        $pulldata = file_get_contents($filetxt);
+        //decode pulled data
+        $decodeddata = json_decode($pulldata);
+        //add new data 
+        $decodeddata[]= $formdata;
+        //encode new data
+        $jsondata= json_encode($decodeddata,JSON_PRETTY_PRINT);
+
+        //put contents in json file
+        if(file_put_contents('todo.json', $jsondata))
+      echo 'Data successfully saved';
+    else echo 'Unable to save data in "todo.json"';
+     }
+ }
+
+?>
+
+
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<title>Todooz</title>
+  <meta charset="utf-8">
 </head>
 <body>
-    <h1>This is like... the BEST todolist...ever</h1>
-    <form action="content.php" method="POST">
-        <input type="text">
-        <input type="submit">
-    </form>
+  <h1>Todooz: The best todo-list ... like ever</h1>
+  <form action="content.php" method="post">
+   Add a task <input type="text" name="todoitem" id="todoitem" /><br />
+     <input type="submit" id="submit" value="Send" />
+  </form>
 </body>
 </html>
