f7fc8d649ba0848fd9be478ffb9e64846d376a69
Thomas Bouchard <thomaswardbouchard@gmail.com>
2018-02-26 18:07:33 +0100

    persistence is key

--- form.php
+++ form.php
@@ -20,10 +20,15 @@
 
         //put contents in json file
         if(file_put_contents('todo.json', $jsondata))
-      echo 'Data successfully saved';
+      echo 'Your todo item has been Todoozed!';
     else echo 'Unable to save data in "todo.json"';
-     }
- }
+     }  
+}
+
+// pull data from json file before adding nex data into it
+        $str = file_get_contents($filetxt);
+        //decode pulled data
+        $jsonTodooz = json_decode($str,true);
 
 ?>
 
@@ -33,12 +38,19 @@
 <head>
 <title>Todooz</title>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
   <h1>Todooz: The best todo-list ... like ever</h1>
-  <form action="content.php" method="post">
+  <form action="form.php" method="post">
    Add a task <input type="text" name="todoitem" id="todoitem" /><br />
      <input type="submit" id="submit" value="Send" />
   </form>
+  <?php 
+  // Loop through colors array
+foreach($jsonTodooz as $value){
+    echo '<div class="todocard">'.$value['todoitem'].'<span class="close">X</span>'.'</div>' . '<br>';
+}
+ ?>
 </body>
 </html>
