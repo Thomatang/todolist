5201ad5cfb53a36ccccab23b98cb342240236cc9
Thomas Bouchard <thomaswardbouchard@gmail.com>
2018-02-26 12:38:30 +0100

    initial commit

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is like... the BEST todolist...ever</h1>
+    <form action="content.php" method="POST">
+        <input type="text">
+        <input type="submit">
+    </form>
+</body>
+</html>
